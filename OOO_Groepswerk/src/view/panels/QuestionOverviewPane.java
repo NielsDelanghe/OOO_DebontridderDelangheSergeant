99b1ebf17c12a514514a543045ca79496db0cd01
Niels Delanghe <niels.delanghe99@gmail.com>
2018-11-24 17:55:00 +0100

    QuestionOverview

--- OOO_Groepswerk/src/view/panels/QuestionOverviewPane.java
+++ OOO_Groepswerk/src/view/panels/QuestionOverviewPane.java
@@ -1,5 +1,7 @@
 package view.panels;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,10 +12,26 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import model.Category;
+import model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionOverviewPane extends GridPane {
 	private TableView table;
 	private Button btnNew;
+
+	private ObservableList<Question> data = FXCollections.observableArrayList();
+
+	private List<String>answers_q1 = new ArrayList<>();
+	private List<String>answers_q2 = new ArrayList<>();
+
+	private Category cat1 = new Category("Design principles","The SOLID design principles",4,"You should know the design of a principle");
+	private Category cat2  = new Category("Design patterns","A design pattern",3,"Know all patterns");
+
+	private Question q1;
+	private Question q2;
 	
 	public QuestionOverviewPane() {
 		this.setPadding(new Insets(5, 5, 5, 5));
@@ -31,7 +49,24 @@ public class QuestionOverviewPane extends GridPane {
         descriptionCol.setCellValueFactory(new PropertyValueFactory("category"));
         table.getColumns().add(descriptionCol);
 		this.add(table, 0, 1, 2, 6);
-		
+
+		answers_q1.add("Simple Factory");
+		answers_q1.add("Singleton");
+		answers_q1.add("Strategy");
+
+		answers_q2.add("S");
+		answers_q2.add("O");
+		answers_q2.add("L");
+		answers_q2.add("I");
+		answers_q2.add("D");
+
+		q1 = new Question("What pattern defines a family of algorithmes?",answers_q1,cat2);
+		q2 = new Question("What design principle has the least to do with Strategys?",answers_q2,cat1);
+
+		data.add(q1);
+		data.add(q2);
+
+		table.setItems(data);
 		btnNew = new Button("New");
 		this.add(btnNew, 0, 11, 1, 1);
 	}
