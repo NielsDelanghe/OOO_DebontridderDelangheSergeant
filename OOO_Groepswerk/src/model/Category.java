293660ab076172e85be0db150a120a984a40713d
Niels Delanghe <niels.delanghe99@gmail.com>
2018-11-24 16:13:33 +0100

    Category

--- OOO_Groepswerk/src/model/Category.java
+++ OOO_Groepswerk/src/model/Category.java
@@ -1,4 +1,51 @@
 package model;
 
 public class Category {
+
+    private String name;
+    private String description;
+
+    public Category(String n, String desc)
+    {
+        this.setName(n);
+        this.setDescription(desc);
+    }
+
+    private void setName(String n)
+    {
+        if(name==null || name.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Name is empty");
+        }
+        else
+            this.name=n;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    private void setDescription(String desc)
+    {
+        if(desc==null || desc.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Description is empty");
+        }
+
+        else
+            this.description=desc;
+
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public String toString()
+    {
+       return name + "\t" + description + "\n";
+    }
+
 }
