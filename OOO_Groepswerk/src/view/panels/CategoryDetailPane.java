6be69b91da4eb645cef3e031003fb83419e58661
BramDebontridder <bram.debontridder@student.ucll.be>
2018-12-01 15:59:41 +0100

    CategoryDetail en Overview

--- OOO_Groepswerk/src/view/panels/CategoryDetailPane.java
+++ OOO_Groepswerk/src/view/panels/CategoryDetailPane.java
@@ -6,7 +6,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -17,44 +16,40 @@ public class CategoryDetailPane extends GridPane {
 	private TextField titleField, descriptionField;
 	private ComboBox categoryField;
 	private CategoryList categories;
-	private ObservableList<String> categorieTitles;
-	private Category c;
+	private ObservableList<String> categoryTitles;
+	private Category category;
 
-
-
-	public CategoryDetailPane(CategoryList categoryList) {
-
-		categories=categoryList;
-		categorieTitles = FXCollections.observableArrayList(categories.getCategoryNames());
+	public CategoryDetailPane(CategoryList categories) {
+		this.categories = categories;
+		this.categoryTitles = FXCollections.observableArrayList(categories.getCategoryNames());
 
 		this.setPrefHeight(150);
 		this.setPrefWidth(300);
-		
 		this.setPadding(new Insets(5, 5, 5, 5));
 		this.setVgap(5);
 		this.setHgap(5);
-
+		//----------------------------------------------------------------------
 		this.add(new Label("Title:"), 0, 0, 1, 1);
 		titleField = new TextField();
 		this.add(titleField, 1, 0, 1, 1);
-
+		//----------------------------------------------------------------------
 		this.add(new Label("Description:"), 0, 1, 1, 1);
 		descriptionField = new TextField();
 		this.add(descriptionField, 1, 1, 1, 1);
-
+		//----------------------------------------------------------------------
 		this.add(new Label("Main Category:"), 0, 2, 1, 1);
 		categoryField = new ComboBox<>();
 		this.add(categoryField, 1, 2, 1, 1);
-		categoryField.setItems(categorieTitles);
-
+		categoryField.setItems(categoryTitles);
+		//----------------------------------------------------------------------
 		btnCancel = new Button("Cancel");
 		this.add(btnCancel, 0, 3, 1, 1);
-
+		//----------------------------------------------------------------------
 		btnOK = new Button("Save");
 		btnOK.isDefaultButton();
 		this.add(btnOK, 1, 3, 1, 1);
+		//----------------------------------------------------------------------
 		setSaveAction(new AddCategory());
-		btnOK.setOnAction(new AddCategory());
 		setCancelAction(new Cancel());
 	}
 
@@ -66,22 +61,18 @@ public class CategoryDetailPane extends GridPane {
 		btnCancel.setOnAction(cancelAction);
 	}
 
-
-
 	public class AddCategory implements EventHandler<ActionEvent>
 	{
-
 		@Override
 		public void handle(ActionEvent event) {
 			try {
-				c = new Category(titleField.getText(), descriptionField.getText());
-				categories.addCategory(c);
-
+				category = new Category(titleField.getText(), descriptionField.getText());
+				categories.addCategory(category);
 				titleField.setText("");
 				descriptionField.setText("");
-				categorieTitles.add(c.getName());
-				System.out.println(categories.getCategotyList().toString());
-			}catch(Exception ex){
+				categoryTitles.add(category.getName());
+			}
+			catch(Exception ex) {
 				Alert alert = new Alert(Alert.AlertType.INFORMATION);
 				alert.setHeaderText("ERROR");
 				alert.setContentText(ex.getMessage());
@@ -92,14 +83,12 @@ public class CategoryDetailPane extends GridPane {
 
 	public class Cancel implements EventHandler<ActionEvent>
 	{
-
 		@Override
 		public void handle(ActionEvent event) {
 			Stage stage =(Stage) btnCancel.getScene().getWindow();
 			stage.close();
 		}
 	}
-
 }
 
 
--- OOO_Groepswerk/src/view/panels/CategoryOverviewPane.java
+++ OOO_Groepswerk/src/view/panels/CategoryOverviewPane.java
@@ -1,7 +1,5 @@
 package view.panels;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -15,49 +13,42 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import model.Category;
 import controller.CategoryList;
-
-import java.util.ArrayList;
-
 
 public class CategoryOverviewPane extends GridPane{
 	private TableView table;
 	private Button btnNew;
 	private CategoryList categories;
-	private ObservableList<Category> data;
 	
-	public CategoryOverviewPane(CategoryList cats) {
-		categories=cats;
-		data = cats.getCategotyList();
+	public CategoryOverviewPane(CategoryList categories) {
+		this.categories = categories;
+
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
         this.setHgap(5);
+
 		this.add(new Label("Categories:"), 0, 0, 1, 1);
 
 		table = new TableView<>();
 		table.setPrefWidth(REMAINING);
+		//----------------------------------------------------------------------
         TableColumn nameCol = new TableColumn<>("Name");
         nameCol.setCellValueFactory(new PropertyValueFactory("name"));
         table.getColumns().add(nameCol);
-
-
+		//----------------------------------------------------------------------
         TableColumn descriptionCol = new TableColumn<>("Description");
         descriptionCol.setCellValueFactory(new PropertyValueFactory("description"));
         table.getColumns().add(descriptionCol);
+		//----------------------------------------------------------------------
 		this.add(table, 0, 1, 2, 6);
-		
 		btnNew = new Button("New");
 		this.add(btnNew, 0, 11, 1, 1);
 		setNewAction(new NewCategory());
+		//----------------------------------------------------------------------
 		table.setItems(categories.getCategotyList());
-
 	}
 	
-	public void setNewAction(EventHandler<ActionEvent> newAction) {
-
-		btnNew.setOnAction(newAction);
-	}
+	public void setNewAction(EventHandler<ActionEvent> newAction) { btnNew.setOnAction(newAction); }
 	
 	public void setEditAction(EventHandler<MouseEvent> editAction) {
 		table.setOnMouseClicked(editAction);
@@ -68,23 +59,17 @@ public class CategoryOverviewPane extends GridPane{
 		return categories;
 	}
 
-
 	private class NewCategory implements EventHandler<ActionEvent>
 	{
-
 		@Override
 		public void handle(ActionEvent event) {
-
 			CategoryDetailPane categoryDetailPane = new CategoryDetailPane(categories);
 			Stage newCategoryStage = new Stage();
-
 			Group root = new Group();
 			Scene categoryScene = new Scene(root,250,150);
-
 			root.getChildren().add(categoryDetailPane);
 			newCategoryStage.setScene(categoryScene);
 			newCategoryStage.show();
 		}
 	}
-
 }
