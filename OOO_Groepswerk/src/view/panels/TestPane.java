908e355f63c7d3c2249032205cad87025dac067d
BramDebontridder <bram.debontridder@student.ucll.be>
2018-12-11 16:08:08 +0100

    TestPane een beetje opgeruimd

--- OOO_Groepswerk/src/view/panels/TestPane.java
+++ OOO_Groepswerk/src/view/panels/TestPane.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import controller.DBContext;
 import db.QuestionTXT;
@@ -64,23 +63,10 @@ public class TestPane extends GridPane {
 		setProcessAnswerAction(new ProcessAnswerAction());
 	}
 
-	public void setProcessAnswerAction(EventHandler<ActionEvent> processAnswerAction) {
-		submitButton.setOnAction(processAnswerAction);
-	}
+	private void setProcessAnswerAction(EventHandler<ActionEvent> processAnswerAction) {submitButton.setOnAction(processAnswerAction);}
+	private void setCloseAction(EventHandler<ActionEvent> closeAction) { submitButton.setOnAction(closeAction); }
 
-	public List<String> getSelectedStatements() {
-		List<String> selected = new ArrayList<String>();
-		if(statementGroup.getSelectedToggle()!=null){
-			selected.add(statementGroup.getSelectedToggle().getUserData().toString());
-		}
-		return selected;
-	}
-
-	public void setCloseAction(EventHandler<ActionEvent> closeAction) {
-		submitButton.setOnAction(closeAction);
-	}
-
-	public class Close implements EventHandler<ActionEvent>
+	class Close implements EventHandler<ActionEvent>
 	{
 		@Override
 		public void handle(ActionEvent event) {
@@ -92,76 +78,34 @@ public class TestPane extends GridPane {
 	class ProcessAnswerAction implements EventHandler<ActionEvent>
 	{
 		@Override
-		public void handle(ActionEvent event) {
-
+		public void handle(ActionEvent event)
+		{
+			//If category doesn't exist in test, add category to test
 			if(!test.getMap().containsKey(test.getFirstQuestion().getCategory()))
 			{
 				test.addKey(test.getFirstQuestion().getCategory());
 			}
-
+			//Set the first answer as the correct answer
 			if(statementGroup.getSelectedToggle().getUserData().equals(test.getFirstQuestion().getPossible_answers().get(0)))
 			{
 				test.getFirstQuestion().setCorrect(true);
 				test.addPointsToCategory(test.getFirstQuestion().getCategory());
 			}
-
 			test.addQuestionToList(test.getFirstQuestion());
 
+			//Clear everything on the screen
 			getChildren().clear();
 			answers.clear();
 			statementGroup = new ToggleGroup();
-			if(test.hasNext()) {
-				//change question
-				questionField.setText("Question: " + test.ChangeQuestionAndGetNext().getQuestion());
-				add(questionField,0,0);
+			//----------------------------------
 
-				//update radiobuttons for new question
-				answers.addAll(test.getFirstQuestion().getPossible_answers());
-				for(int i = 0; i < answers.size(); i++)
-				{
-					RadioButton button = new RadioButton(answers.get(i));
-					button.setUserData(answers.get(i));
-					radioButtons.add(button);
-					button.setToggleGroup(statementGroup);
-					add(button,0,i+1);
-					setProcessAnswerAction(new ProcessAnswerAction());
-				}
-				add(submitButton,0,10,1,1);
+			if(test.hasNext())
+			{
+				loadNextQuestion();
 			}
 			else
 			{
-				int score =0;
-				for(Question question : test.getQuestions())
-				{
-					if(question.getCorrect()==true)
-					{
-						score++;
-					}
-				}
-				//Load property file
-				Properties properties = new Properties();
-				InputStream is = null;
-				try {
-					File file = new File("evaluation.properties");
-					is = new FileInputStream(file);
-				}
-				catch ( Exception e ) { is = null; }
-				try {
-					if ( is == null ) {
-						is = getClass().getResourceAsStream("evaluation.properties");
-					}
-					properties.load( is );
-				}
-				catch ( Exception e ) { }
-				//--------------------------------------------------
-				String evaluationMode = properties.getProperty("evaluation.mode", "score");
-				switch (evaluationMode)
-				{
-					case "score": showScore(score); break;
-					case "feedback": showFeedback(); break;
-					default: showScore(score); break;
-				}
-				add(questionField,0,0);
+				showCorrectEvaluation();
 			}
 		}
 		private void showFeedback()
@@ -169,7 +113,7 @@ public class TestPane extends GridPane {
 			String feedback = "";
 			for(Question question : test.getQuestions())
 			{
-				if(question.getCorrect()==false)
+				if(question.getCorrect() == false)
 				{
 					feedback += question.getQuestion() + "\n\t" + question.getFeedback()+"\n\n";
 				}
@@ -183,6 +127,65 @@ public class TestPane extends GridPane {
 		private void showScore(int score)
 		{
 			questionField.setText("Your score is: " + score +"/"+ test.getQuestions().size() + "\n" + test.getCategorieAndPoints());
+		}
+
+		private void loadNextQuestion()
+		{
+			//change question
+			questionField.setText("Question: " + test.ChangeQuestionAndGetNext().getQuestion());
+			add(questionField,0,0);
+
+			//update radiobuttons for new question
+			answers.addAll(test.getFirstQuestion().getPossible_answers());
+			for(int i = 0; i < answers.size(); i++)
+			{
+				RadioButton button = new RadioButton(answers.get(i));
+				button.setUserData(answers.get(i));
+				radioButtons.add(button);
+				button.setToggleGroup(statementGroup);
+				add(button,0,i+1);
+				setProcessAnswerAction(new ProcessAnswerAction());
+			}
+			add(submitButton,0,10,1,1);
+		}
+
+		private void showCorrectEvaluation()
+		{
+			int score = 0;
+			for(Question question : test.getQuestions())
+			{
+				if(question.getCorrect()==true)
+				{
+					score++;
+				}
+			}
+			//Load property file
+			Properties properties = new Properties();
+			InputStream is = null;
+			try {
+				File file = new File("evaluation.properties");
+				is = new FileInputStream(file);
+			}
+			catch ( Exception e ) { is = null; }
+			try {
+				if ( is == null ) {
+					is = getClass().getResourceAsStream("evaluation.properties");
+				}
+				properties.load( is );
+			}
+			catch ( Exception e ) { }
+			//--------------------------------------------------
+			String evaluationMode = properties.getProperty("evaluation.mode", "score");
+			switch (evaluationMode)
+			{
+				case "score": showScore(score); break;
+				case "feedback": showFeedback(); break;
+				default: showScore(score); break;
+			}
+			add(questionField,0,0);
+			submitButton = new Button("Close");
+			add(submitButton,0,10,1,1);
+			setCloseAction(new Close());
 		}
 	}
 }
